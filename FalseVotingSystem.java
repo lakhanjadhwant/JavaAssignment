d0cc6ba3088d77b4afb3473bab925290221883f8
lakhanjadhwant <98732101+lakhanjadhwant@users.noreply.github.com>
2023-12-01 13:40:39 +0530

    Updated FalseVotingSystem.java

--- FalseVotingSystem.java
+++ FalseVotingSystem.java
@@ -19,88 +19,60 @@ public class FalseVotingSystem {
             System.out.println("Enter\n1 - Cast Vote\n2 - Candidate wise Result\n3 - Comprehensive Report\n4 - Exit ");
             int choice = sc.nextInt();
 
-            if (count % 3 == 0 || count % 7 == 0) {
-                processVoteCasting(choice, sc, true);
-            } else {
-                processVoteCasting(choice, sc, false);
+            switch (choice) {
+                case 1:
+                    castVote(sc);
+                    break;
+                case 2:
+                    showCandidateResult(sc);
+                    break;
+                case 3:
+                    showComprehensiveReport();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+                    count--;
             }
+
+            System.out.println("*****************************");
         }
     }
 
-    private static void processVoteCasting(int choice, Scanner sc, boolean isSpecialRound) {
-        switch (choice) {
-            case 1:
-                castVote(sc, isSpecialRound);
-                break;
-            case 2:
-                showCandidateResult(sc);
-                break;
-            case 3:
-                showComprehensiveReport();
-                break;
-            case 4:
-                exitProgram();
-                break;
-            default:
-                System.out.println("Invalid choice. Please select a valid option.");
-                count--;
-        }
-    }
-
-    private static void castVote(Scanner sc, boolean isSpecialRound) {
+    private static void castVote(Scanner sc) {
         System.out.println("Press 1 for A\nPress 2 for B\nPress 3 for C\nPress 4 for D\n");
         int press = sc.nextInt();
 
-        if (isSpecialRound) {
-            if(press==1){
+        if (press >= 1 && press <= 4) {
+            if (count % 3 == 0 || count % 7 == 0) {
                 C++;
-                System.out.println("You vote for A..");
+                System.out.println("You vote for " + (char) ('A' + press - 1));
+            } else {
+                switch (press) {
+                    case 1:
+                        A++;
+                        System.out.println("You vote for A");
+                        break;
+                    case 2:
+                        B++;
+                        System.out.println("You vote for B");
+                        break;
+                    case 3:
+                        C++;
+                        System.out.println("You vote for C");
+                        break;
+                    case 4:
+                        D++;
+                        System.out.println("You vote for D");
+                        break;
+                }
             }
-            else if(press==2){
-                C++;
-                System.out.println("You vote for B..");
-            }
-            else if(press==3){
-                C++;
-                System.out.println("You vote for C..");
-            }
-            else if(press==4){
-                C++;
-                System.out.println("You vote for D..");
-            }
-            else{
-                System.out.println("Invalid vote. Please vote for A, B, C, or D...");
-                count--;
-            }
-
-            
         } else {
-            switch (press) {
-                case 1:
-                    A++;
-                    System.out.println("You vote for A");
-                    break;
-                case 2:
-                    B++;
-                    System.out.println("You vote for B");
-                    break;
-                case 3:
-                    C++;
-                    System.out.println("You vote for C");
-                    break;
-                case 4:
-                    D++;
-                    System.out.println("You vote for D");
-                    break;
-                default:
-                    System.out.println("Invalid vote. Please vote for A, B, C, or D.");
-                    count--;
-                    return;
-            }
+            System.out.println("Invalid vote. Please vote for A, B, C, or D...");
+            count--;
         }
-
-        
-        System.out.println("*****************************");
     }
 
     private static void showCandidateResult(Scanner sc) {
@@ -133,11 +105,5 @@ public class FalseVotingSystem {
         System.out.println("Total Votes of A = " + A + "\nTotal Votes of B = " + B +
                 "\nTotal Votes of C = " + C + "\nTotal Votes of D = " + D);
         System.out.println("*****************************");
-    }
-
-    private static void exitProgram() {
-        count--;
-        System.out.println("Exiting the program.");
-        System.exit(0);
     }
 }
